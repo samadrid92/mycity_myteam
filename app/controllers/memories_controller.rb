e920bf6c298a125a3a25c6ef6ed63a7578540063
samadrid92 <YOUR_GITHUB_EMAIL_ADDRESS>
2016-05-31 14:48:17 -0700

    create a memory works

--- app/controllers/memories_controller.rb
+++ app/controllers/memories_controller.rb
@@ -1,2 +1,46 @@
 class MemoriesController < ApplicationController
+  before_action :set_team
+
+  def index
+    @memories = Memory.all
+  end
+
+  def show
+  end
+
+  def new
+    @memory = Memory.new
+  end
+
+  def create
+    @team = @city.teams.find_by_id(params[:team_id])
+    @memory = @team.memories.new(memories_params)
+    @memory.user_id = current_user.id
+    if @memory.save
+      redirect_to city_team_path(@city, @team)
+    else
+      redirect_to root_path
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_team
+      @city = City.find(params[:city_id])
+      @team = @city.teams.find_by_id(params[:team_id])
+  end
+
+  def memories_params
+    params.require(:memory).permit(:name, :description, :age, :date, :hometown, :team_id, :user_id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,14 @@ Rails.application.routes.draw do
   patch '/cities/:city_id/teams/:team_id', to: 'teams#update'
   delete '/cities/:city_id/teams/:team_id', to: 'teams#destroy', as: 'destroy_team'
 
+  get '/cities/:city_id/teams/:team_id/memories', to: 'memories#index', as: 'team_memories'
+  get '/cities/:city_id/teams/:team_id/memories/new', to: 'memories#new', as: 'new_team_memory'
+  post '/cities/:city_id/teams/:team_id/memories', to: 'memories#create', as: 'city_team_memories'
+  get '/cities/:city_id/teams/:team_id/memories/:memory_id', to: 'memories#show', as: 'team_memory'
+  get '/cities/:city_id/teams/:team_id/memories/memory_id/edit', to: 'memories#edit', as: 'edit_team_memory'
+  patch '/cities/:city_id/teams/:team_id/memories/memory_id', to: 'memories#update'
+  delete '/cities/:city_id/teams/:team_id/memories/memory_id', to: 'memories#destroy', as: 'destroy_team_memory'
+
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
